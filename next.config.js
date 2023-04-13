dd15a912f0e4ce263b0e66357583b993ad2800b2
jemmec <jwmcdade96@gmail.com>
2023-04-13 14:41:46 +0930

    Added PUBLIC_URL environment variable for assetPrefixing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.PUBLIC_URL ? `/${process.env.PUBLIC_URL}/` : '/',
 }
 
 module.exports = nextConfig
