93bb07c341b06a43fffd311999413a8e79803a33
jemmec <spogjem@gmail.com>
2023-04-16 17:47:01 +0930

    Excluded socket-server from next build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: process.env.PUBLIC_URL ? `/${process.env.PUBLIC_URL}/` : '/',
+  tsconfigPath: "tsconfig.json"
 }
 module.exports = nextConfig
