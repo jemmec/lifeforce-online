6911d4c91c87f66aac200f75fbf8f2882a2881b7
jemmec <jwmcdade96@gmail.com>
2023-04-18 23:41:23 +0930

    Small style tweaks

--- src/components/small-text.tsx
+++ src/components/small-text.tsx
@@ -1,18 +1,19 @@
 export function SmallText() {
-    return (
-      <>
-        <div className='small-text'>
-          {`Lifeforce is an open source project created by @jemmec. Sit minim cupidatat in duis esse do. Ad exercitation tempor ipsum Lorem anim excepteur elit ut nulla nostrud. Quis eu et nostrud excepteur proident nulla qui ut ullamco dolore excepteur.`}
-        </div>
-        <style jsx>
-          {`
+  return (
+    <>
+      <div className='small-text'>
+        <div>{`Lifeforce © 2023 all rights belong to the person whom rights they belong. Please do not put your hand over an open flame. It is of the express interest of everyone that we remain calm. Try not to put excessive dedorant on in the mornings.`}</div>
+        <div>{`Made by `}<a href="https://github.com/jemmec">{`@jemmec`}</a></div>
+      </div>
+      <style jsx>
+        {`
             .small-text{
               opacity: 0.35;
               font-size: 12px;
               text-align: center;
             }
           `}
-        </style>
-      </>
-    )
-  }
+      </style>
+    </>
+  )
+}
